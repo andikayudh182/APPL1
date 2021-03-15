5a3b6a5d42b78fad864b001c3b9aa66475eff459
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2021-03-15 13:17:22 +0700

    Update 3 - Painting Shapes
    Update 3 - Painting Shapes

---
+++ PaintingShapes/src/paintingshapes/Cylinder.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paintingshapes;
+
+/**
+ *
+ * @author andika19
+ */
+public class Cylinder extends Shape{
+    private double height;
+    private double radius;
+    
+    public Cylinder(double h, double r){
+        super("Cylinder");
+        height = h;
+        radius = r;
+    }
+
+
+    @Override
+    public double area() {
+        return Math.PI*radius*radius*height;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + " of radius " + radius +" and height "+ height;
+    }
+}
+
--- PaintingShapes/src/paintingshapes/Sphere.java
+++ PaintingShapes/src/paintingshapes/Sphere.java
@@ -7,31 +7,25 @@ package paintingshapes;
 
 /**
  *
- * @author ANDIKA YUDHA RIYANTO
+ * @author andika19
  */
-public class Sphere extends Shape 
-{ 
-    private double radius; //radius in feet 
-    //---------------------------------- 
-    // Constructor: Sets up the sphere. 
-    //---------------------------------- 
-    public Sphere(double r) 
-    { 
-    super("Sphere"); 
-    radius = r; 
- } 
- //----------------------------------------- 
- // Returns the surface area of the sphere. 
- //----------------------------------------- 
- public double area() 
- { 
-    return 4*Math.PI*radius*radius; 
- } 
- //----------------------------------- 
- // Returns the sphere as a String. 
- //----------------------------------- 
- public String toString() 
- { 
- return super.toString() + " of radius " + radius; 
- } 
-} 
+
+public class Sphere extends Shape{
+    private double radius;  //in feet
+    
+    public Sphere(double r){
+        super("Sphere");
+        radius = r;
+    }
+    
+    @Override
+    public double area(){
+        return 4*Math.PI*radius*radius;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + " of radius " + radius;
+    }
+    
+}
