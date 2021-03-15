5e25f445a32ce1414ec53ed21b233e603e0d7014
andikayudh182 <61397720+andikayudh182@users.noreply.github.com>
2021-03-15 18:44:48 +0700

    Perapihan header
    Perapihan header

--- TypeEmployee/src/typeemployee/Employee.java
+++ TypeEmployee/src/typeemployee/Employee.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Employee.java
+Andika Yudha Riyanto - 191524034
+D4-2B Teknik Informatika
+Politeknik Negeri Bandung
  */
 package typeemployee;
 
